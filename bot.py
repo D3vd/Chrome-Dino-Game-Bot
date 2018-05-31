6e231509747b1a13cdcd12467361f7a849ded15c
Dev-C137 <dev.91096@gmail.com>
2018-05-31 09:09:30 +0530

    Made Dino jump over obstacles

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import pyautogui
 import time
-
+from PIL import ImageGrab, ImageOps
+from numpy import *
 
 class Coordinates:
     restart_btn = (339,314)
-    dino = (94,324)
+    dino = (90,325)
 
 def restart_game():
     pyautogui.click(Coordinates.restart_btn)
@@ -14,8 +15,20 @@ def jump():
     time.sleep(0.05)
     pyautogui.keyUp('space')
 
+def imageGrab():
+    box = (126,317,194,356 )
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    print(a.sum())
+    return a.sum()
 
 
-restart_game()
-time.sleep(1)
-jump()
+def main():
+    restart_game()
+    while True:
+        if imageGrab() != 2899 :
+            jump()
+            time.sleep(0.2)
+
+main()
